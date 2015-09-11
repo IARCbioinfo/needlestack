c72c0c7b774b338de88f51dd9b869d5b0497e1ab
Matthieu Foll <follm@iarc.fr>
2015-09-11 13:28:32 +0200

    Cutting bed files with zero length regions was not working properly

--- bin/bed_large_cut.r
+++ bin/bed_large_cut.r
@@ -1,11 +1,9 @@
 #! /usr/bin/env Rscript
 nb_pieces=as.numeric(commandArgs(TRUE)[1])
-#bed=read.table("wes_bed_merged.bed",stringsAsFactors = F)
 bed=read.table(pipe('cat /dev/stdin'),stringsAsFactors = F)
-bed_cum_size=cumsum(bed[,3]-bed[,2])
-cut_cum_size=round(seq(1,bed_cum_size[length(bed_cum_size)],l=nb_pieces+1))[-c(1)]
+bed_cum_size=cumsum(bed[,3]-bed[,2]+1)
+cut_cum_size=floor(seq(1,bed_cum_size[length(bed_cum_size)],l=nb_pieces+1))[-c(1)]
 lines_cut_bed=unique(c(0,unlist(lapply(cut_cum_size,function(x){min(which(bed_cum_size>=x))}))))
-size=numeric(nb_pieces)
 for (k in 1:(length(lines_cut_bed)-1)) {
   cur_region=(lines_cut_bed[k]+1):lines_cut_bed[k+1]
   region_name=paste(bed[cur_region[1],1],"_",bed[cur_region[1],2],"-",bed[cur_region[length(cur_region)],1],"_",bed[cur_region[length(cur_region)],3],sep="")
