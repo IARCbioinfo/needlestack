86564ed9c2d37170bbadb2bfbcf414eebc204e0a
Matthieu Foll <follm@iarc.fr>
2015-10-14 17:41:33 +0200

    Updated to iarcbioinfo org

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git add .
 git status
 git commit --allow-empty -m "Results from $CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH CircleCI tests build $CIRCLE_BUILD_NUM"
 git push origin master
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/mfoll/robust-regression-caller/trigger/e85e32e7-508d-4971-b621-e1a0409f711a/
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/needlestack/trigger/fcd71f24-0f1e-40b8-b76a-09eb5a39e924/
