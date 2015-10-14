b35bda01ad2438f5d9242d92250f8c85effc527c
Matthieu Foll <follm@iarc.fr>
2015-10-14 16:04:49 +0200

    Update pileup_nbrr_caller_vcf.r

--- bin/pileup_nbrr_caller_vcf.r
+++ bin/pileup_nbrr_caller_vcf.r
@@ -1,5 +1,21 @@
 #! /usr/bin/env Rscript
 
+# needlestack: a multi-sample somatic variant caller
+# Copyright (C) 2015 Matthieu Foll and Tiffany Delhomme
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #library(qvalue)
 #library(discreteMTP)
 glmrob.nb <- function(y,x,bounding.func='T/T',c.tukey.beta=5,c.tukey.sig=3,c.by.beta=4,weights.on.x='none',
