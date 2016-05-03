35e1c0e78f62fcb7fdf16a4adb9099dbaa050e10
Matthieu Foll <follm@iarc.fr>
2016-05-03 10:47:16 +0200

    Removed empty commits in CHANGELOG

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git config --global user.email "follm@iarc.fr"
 git config --global user.name "Circle CI_$CIRCLE_PROJECT_REPONAME_$CIRCLE_BRANCH"
 git add .
 git status
-git commit --allow-empty -m "Added CHANGELOG [ci skip]"
+git commit -m "Added CHANGELOG [ci skip]"
 git push origin $CIRCLE_BRANCH
 
 curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/needlestack/trigger/fcd71f24-0f1e-40b8-b76a-09eb5a39e924/
