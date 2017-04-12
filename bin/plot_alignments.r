e26953136651e57e182ad035611bc24ace049b1f
aurelieGabriel <gabrielaureli7@gmail.com>
2017-04-12 15:39:54 +0200

    Update plot_alignements.r : file header

--- bin/plot_alignments.r
+++ bin/plot_alignments.r
@@ -1,3 +1,21 @@
+#! /usr/bin/env Rscript
+
+# needlestack: a multi-sample somatic variant caller
+# Copyright (C) 2017 Matthieu Foll, Tiffany Delhomme, Nicolas Alcala and Aurelie Gabriel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #loading libraries
 if(do_alignments==TRUE){
   
