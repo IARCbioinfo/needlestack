2722b7267d196d7bb5e3e69c188bff1ebb5dda69
tdelhomme <delhommet@students.iarc.fr>
2018-08-17 17:28:50 +0200

    remove data test to do not commit it

--- bin/needlestack.r
+++ bin/needlestack.r
@@ -19,7 +19,6 @@
 
 print(Sys.time())
 
-library("Gviz")
 ############################## ARGUMENTS SECTION ##############################
 args <- commandArgs(TRUE)
 parseArgs <- function(x) strsplit(sub("^--", "", x), "=")
