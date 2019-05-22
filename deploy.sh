b1c3b584235663d83b34bd8c0a2dbcc763032820
tdelhomme <delhommet@students.iarc.fr>
2019-05-22 11:59:29 +0200

    skip CI when deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git config --global user.email "follm@iarc.fr"
 git config --global user.name "Circle CI_$CIRCLE_PROJECT_REPONAME_$CIRCLE_BRANCH"
 git add .
 git status
-git commit --allow-empty -m "Results from $CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH CircleCI tests build $CIRCLE_BUILD_NUM"
+git commit --allow-empty -m "Results from $CIRCLE_PROJECT_REPONAME:$CIRCLE_BRANCH CircleCI tests build $CIRCLE_BUILD_NUM [skip ci]"
 git push origin master
 
 gem install github_changelog_generator
@@ -14,7 +14,7 @@ git config --global user.email "follm@iarc.fr"
 git config --global user.name "Circle CI_$CIRCLE_PROJECT_REPONAME_$CIRCLE_BRANCH"
 git add .
 git status
-git commit -m "Added CHANGELOG"
+git commit -m "Added CHANGELOG [skip ci]"
 git push origin $CIRCLE_BRANCH
 
 curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/needlestack/trigger/fcd71f24-0f1e-40b8-b76a-09eb5a39e924/
